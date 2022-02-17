916207df77965005bd8d474ec6060f22ca049ec4
zaryab <zaryabsheikh6@gmail.com>
2022-02-17 12:44:44 +0500

    edit app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-//alert("Hello World");
+alert("Hello World");
 
-//var a=43;
-//var b=54;
-//var c=a*b;
-//alert(c);
+var a=43;
+var b=54;
+var c=a*b;
+alert(c);
 
-//var a=32;
-//var b=44;
-//var c=b-a;
-//console.log(c);
+var a=32;
+var b=44;
+var c=b-a;
+console.log(c);
 
 
-//var a="zaryab";
-//var b="shaikh";
-//var c=a+b;
-//alert(c)
+var a="zaryab";
+var b="shaikh";
+var c=a+b;
+alert(c)
 
